28ffa24d8d5231380697ba4ccd49067e3fff7ace
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-10-06 20:23:33 +0500

    Task5

---
+++ Tyuiu.KorotchenkoDM.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KorotchenkoDM.Sprint2.Task5.V4.Lib;
+
+namespace Tyuiu.KorotchenkoDM.Sprint2.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Коротченко Д. М. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Коротченко Данил Михайлович | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат                               *");
+            Console.WriteLine("\n* Условие: Мастям игральных карт условно присвоены следующие порядковые   *");
+            Console.WriteLine("* номера: масти <<пики>> - 1, масти <<трефы>> - 2, масти <<бубны>> - 3,   *");
+            Console.WriteLine("* масти <<червы>> - 4. По заданному номеру масти m (1<= m <=4) определить *");
+            Console.WriteLine("* название соответстующей масти                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер масти: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            string res;
+            if ((value < 1)||(value > 4))
+                res = "Введено неверное значение";
+            else
+                res = "Это карта: " + ds.FindCardSuit(value);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
