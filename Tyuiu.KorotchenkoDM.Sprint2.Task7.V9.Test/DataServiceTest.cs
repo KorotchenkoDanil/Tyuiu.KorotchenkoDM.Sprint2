45ae1923a2b69923c8b727fe20a3eb7f98adbceb
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-10-07 22:07:50 +0500

    Task7

---
+++ Tyuiu.KorotchenkoDM.Sprint2.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KorotchenkoDM.Sprint2.Task7.V9.Lib;
+
+namespace Tyuiu.KorotchenkoDM.Sprint2.Task7.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            double x = Math.PI / 12;
+            double y = 0.4;
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = false;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KorotchenkoDM.Sprint2.Task7.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorotchenkoDM.Sprint2.Task7.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorotchenkoDM.Sprint2.Task7.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aaa2acd8-5f9c-421b-8110-6c5d9de149f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
